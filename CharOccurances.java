104da3a285f492de5f3ec69f98f4bc772e20b3c3
67887887 <95364746+67887887@users.noreply.github.com>
2021-12-01 21:00:34 +0530

    Add files via upload

---
+++ CharOccurances.java
@@ -0,0 +1,16 @@
+package week1.weekdayass;
+
+public class CharOccurances {
+
+	public static void main(String[] args) {
+		String str = "Welcome to Chennai";
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'e') {
+				count++;
+			}
+		}
+		System.out.println("Frequency of e is: " + count);
+	}
+
+}
---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package week1.weekdayass;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		String palindrome = "Madam";
+		String rev = "";
+
+		for (int i = palindrome.length() - 1; i >= 0; i--) {
+			rev = rev + palindrome.charAt(i);
+			}
+
+			if(palindrome.equalsIgnoreCase(rev))
+	        {
+	            System.out.println("The string is palindrome.");
+	        }
+	        else
+	        {
+	            System.out.println("The string is not palindrome.");
+	        }
+		}
+
+	
+}
